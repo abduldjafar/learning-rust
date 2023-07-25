400c9eb60cf1bf2f7008a1d711693d7a1ece22e2
Abdul Haris Djafar <abdul.djafar@quipper.com>
2023-07-25 10:44:33 +0700

    update

--- duckdb-integration/src/main.rs
+++ duckdb-integration/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use duckdb::{Connection, Result};
+
+fn main() -> Result<()> {
+    let conn = Connection::open_in_memory()?;
+
+    conn.execute_batch(
+        r"INSTALL json;
+        LOAD json;
+        CREATE TABLE asoi as (
+            select * from read_ndjson('data.json')
+        );
+         ")?;
+
+    Ok(())
 }
---
+++ ktools/src/tests/os_mmonitoring.rs
@@ -0,0 +1,1 @@
+use  os_monitoring::Monitoring;
