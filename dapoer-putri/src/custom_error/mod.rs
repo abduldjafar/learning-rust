279bcdec58b9dcd4165f37aa3d5e09e25246aac0
Abdul Haris Djafar <abdul.djafar@quipper.com>
2023-07-29 06:17:41 +0700

    clean code

--- dapoer-putri/src/custom_error/mod.rs
+++ dapoer-putri/src/custom_error/mod.rs
@@ -1,5 +1,6 @@
 use std::fmt;
-
+use mongodb::error::Error as MongoError;
+use mongodb::bson::oid::Error as BsonOidError;
 
 pub const ERROR_DATABASE_CONNECTION: &str = "getting error connection to database";
 pub const ERROR_GET_DATA_FROM_DB: &str = "getting error when collect data from database";
@@ -13,5 +14,21 @@ pub struct CustomError{
 impl fmt::Display for CustomError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "error with  message : {}",self.message)
+    }
+}
+
+impl From<MongoError> for CustomError {
+    fn from(error: MongoError) -> Self {
+        CustomError {
+            message: error.to_string(),
+        }
+    }
+}
+
+impl From<BsonOidError> for CustomError {
+    fn from(error: BsonOidError) -> Self {
+        CustomError {
+            message: error.to_string(),
+        }
     }
 }
--- dapoer-putri/src/data_sources/mongo_source.rs
+++ dapoer-putri/src/data_sources/mongo_source.rs
@@ -1,15 +1,12 @@
-use std::env;
-
-use crate::custom_error::{CustomError, ERROR_DATABASE_CONNECTION, ERROR_GET_DATA_FROM_DB};
-
 use super::{data_sources::Datasource, User};
+use crate::custom_error::CustomError;
 use async_trait::async_trait;
 use mongodb::{
     bson::{self, doc, oid::ObjectId, Document},
-    error::Error,
     options::{ClientOptions, FindOneOptions},
     Client,
 };
+use std::env;
 
 enum Table {
     User,
@@ -21,95 +18,58 @@ enum Database {
 
 pub struct MongoSource {}
 
-fn choose_table(table: Table) -> String {
-    match table {
-        Table::User => "users".to_string(),
+impl MongoSource {
+    async fn get_collection(
+        &self,
+        table: Table,
+    ) -> Result<mongodb::Collection<Document>, CustomError> {
+        let conn = connection().await?;
+        let database_name = match Database::DapoerPoetri {
+            Database::DapoerPoetri => "dapoer-poetri",
+        };
+        let collection_name = match table {
+            Table::User => "users",
+        };
+        Ok(conn.database(database_name).collection(collection_name))
     }
 }
 
-fn choose_database(database: Database) -> String {
-    match database {
-        Database::DapoerPoetri => "dapoer-poetri".to_string(),
-    }
-}
-
-async fn connection() -> Result<Client, Error> {
+async fn connection() -> Result<Client, CustomError> {
     let client_uri =
         env::var("MONGODB_URI").expect("You must set the MONGODB_URI environment var!");
-
     let client_options = ClientOptions::parse(client_uri).await?;
+    let client = Client::with_options(client_options)?;
 
-    // Create a new client and connect to the server
-    let client = Client::with_options(client_options);
-    client
+    Ok(client)
 }
 
 #[async_trait]
 impl Datasource for MongoSource {
-    async fn get_user(&self, id: String) -> Result<Option<super::User>, CustomError> {
-        let conn_result = connection().await;
+    async fn get_user(&self, id: String) -> Result<Option<User>, CustomError> {
+        let collection = self.get_collection(Table::User).await?;
 
-        let conn = match conn_result {
-            Ok(conn) => conn,
-            Err(_) => return Err(CustomError{
-                message: ERROR_DATABASE_CONNECTION.to_string()
-            }),
+        let object_id = ObjectId::parse_str(&id)?;
+
+        let query = doc! {
+            "_id": object_id
         };
 
-        let collection: mongodb::Collection<Document> = conn
-            .database(&choose_database(Database::DapoerPoetri))
-            .collection(&choose_table(Table::User));
-
-        let object_id_tmp = ObjectId::parse_str(&id);
-
-        match object_id_tmp {
-            Ok(id) => {
-                let query = doc! {
-                    "_id": id
-                };
-
-                let options = FindOneOptions::builder().build();
-
-                let bson_doc_result = collection.find_one(query, options).await;
-
-                let bson_doc = match bson_doc_result {
-                    Ok(bson_result) => bson_result,
-                    Err(_) => return Err(CustomError{
-                        message:ERROR_GET_DATA_FROM_DB.to_string()
-                    }),
-                };
-
-                let bson_bytes = bson::to_vec(&bson_doc).unwrap();
-
-                let user: User = bson::from_slice(&bson_bytes).unwrap();
-
-                Ok(Some(user))
-            }
-            Err(_) => Ok(None),
+        let options = FindOneOptions::builder().build();
+        if let Some(doc) = collection.find_one(query, options).await? {
+            Ok(Some(bson::from_document(doc).unwrap()))
+        } else {
+            Ok(None)
         }
-
     }
 
-    async fn get_users(&self) -> Vec<super::User> {
+    async fn get_users(&self) -> Vec<User> {
         todo!()
     }
 
     async fn add_user(&self, user: User) -> Result<(), CustomError> {
-        let conn_result = connection().await;
-
-        let conn = match conn_result {
-            Ok(conn) => conn,
-            Err(_) => return Err(CustomError{
-                message: ERROR_DATABASE_CONNECTION.to_string()
-            }),
-        };
-
-        let collection: mongodb::Collection<Document> = conn
-            .database(&choose_database(Database::DapoerPoetri))
-            .collection(&choose_table(Table::User));
-
+        let collection = self.get_collection(Table::User).await?;
         let bson_user = bson::to_document(&user).expect("Failed to convert to BSON Document");
-        let _ = collection.insert_one(bson_user, None).await;
+        collection.insert_one(bson_user, None).await?;
         Ok(())
     }
 
