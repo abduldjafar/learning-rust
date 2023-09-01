29a7cd61bb9b2f085624c79f58277ee73269d584
Abdul Haris Djafar <abdul.djafar@quipper.com>
2023-09-01 10:22:46 +0700

    add storage writer integration

---
+++ ingestion-tools/src/mongo_operation.rs
@@ -0,0 +1,91 @@
+use bson::{doc, Bson};
+use futures::StreamExt;
+use mongodb::{
+    bson::{self, oid::ObjectId, Document}, Collection,
+};
+use rayon::prelude::*;
+
+use crate::writer::{ DataWriter, GcsStorage};
+
+
+
+pub async fn get_split_keys(
+    db: mongodb::Database,
+    database: String,
+    collection: String,
+    batch_size_in_mb: i32,
+) -> Result<Vec<(ObjectId, ObjectId)>, Box<dyn std::error::Error>> {
+
+    let split_vector_command = doc! {
+        "splitVector": format!("{}.{}", database, collection),
+        "keyPattern": doc! { "_id": Bson::Int32(1) },
+        "force": Bson::Boolean(false),
+        "maxChunkSize": batch_size_in_mb
+    };
+
+    let result = db.run_command(split_vector_command, None).await?;
+    let option_bson = result.get("splitKeys");
+
+    let unwrapped_keys: Option<Vec<ObjectId>> = option_bson.and_then(|bson| {
+        bson.as_array().map(|array| {
+            array
+                .par_iter() // Use par_iter() to iterate in parallel
+                .filter_map(|item| {
+                    if let Bson::Document(doc) = item {
+                        doc.get("_id").and_then(Bson::as_object_id)
+                    } else {
+                        None
+                    }
+                })
+                .collect()
+        })
+    });
+
+    let unwrapped_keys = unwrapped_keys.unwrap_or_default();
+
+    let tuple_vector: Vec<(ObjectId, ObjectId)> = unwrapped_keys
+        .par_iter()
+        .zip(unwrapped_keys.par_iter().skip(1))
+        .map(|(&current_key, &next_key)| (current_key.clone(), next_key.clone()))
+        .collect();
+
+    Ok(tuple_vector)
+
+}
+
+pub async fn get_mongo_datas(
+    tuple_object_id: (ObjectId, ObjectId),
+    conn: Collection<Document>,
+    output: String,
+    index: i32
+) -> Result<(), Box<dyn std::error::Error>> {
+    let query = doc! {
+        "_id": {
+            "$gte": tuple_object_id.0,
+            "$lt": tuple_object_id.1
+        }
+    };
+
+    let mut datas: String = Default::default() ;
+    let gcs_storage = GcsStorage{
+        bucket:String::from("quipper-fact-dev"),
+    };
+
+    let writer = DataWriter::new(gcs_storage);
+    
+    let mut cursor = conn.find(query, None).await?;
+
+    println!("processing batch {:?}...", index);
+
+    while let Some(doc) = cursor.next().await {
+        let real_doc = doc?;
+        let json_str = serde_json::to_string(&real_doc)?;
+
+        datas.push_str(&format!("{}\n", json_str));
+    }
+
+    writer.write_data(&format!("data/from_rust/{}_{}.json",&output,index), &datas).await?;
+
+
+    Ok(())
+}
---
+++ ingestion-tools/src/writer.rs
@@ -0,0 +1,76 @@
+use async_trait::async_trait;
+use google_cloud_storage::{
+    client::{Client, ClientConfig},
+    http::objects::upload::{Media, UploadObjectRequest, UploadType},
+};
+use std::fs::File;
+use std::io::Write;
+
+// Define a trait for the storage provider
+#[async_trait]
+pub trait StorageProvider {
+    async fn write(&self, path: &str, data: &str) -> Result<(), Box<dyn std::error::Error>>;
+}
+
+// Implement GCS storage
+pub struct GcsStorage{
+    pub bucket: String,
+}
+
+#[async_trait]
+impl StorageProvider for GcsStorage {
+    async fn write(&self, path: &str, data: &str) -> Result<(), Box<dyn std::error::Error>> {
+        // Implement GCS write logic here
+        let config = ClientConfig::default().with_auth().await?;
+        let client = Client::new(config);
+
+        let upload_type = UploadType::Simple(Media::new(path.clone().to_string()));
+        client
+            .upload_object(
+                &UploadObjectRequest {
+                    bucket: self.bucket.clone(),
+                    ..Default::default()
+                },
+                data.clone().to_string(),
+                &upload_type,
+            )
+            .await?;
+
+        println!("Writing to GCS path: {}", path);
+        Ok(())
+    }
+}
+
+// Implement local file system storage
+pub struct LocalStorage;
+
+#[async_trait]
+impl StorageProvider for LocalStorage {
+    async fn write(&self, path: &str, data: &str) -> Result<(), Box<dyn std::error::Error>> {
+        // Implement local file write logic here
+        let mut file = File::create(path)?;
+        file.write_all(data.as_bytes())?;
+        println!("Writing to local path: {}", path);
+        Ok(())
+    }
+}
+
+// Main struct for writing data using StorageProvider
+pub struct DataWriter<T: StorageProvider> {
+    storage: T,
+}
+
+impl<T: StorageProvider> DataWriter<T> {
+    pub fn new(storage: T) -> Self {
+        DataWriter { storage }
+    }
+
+    pub async fn write_data(
+        &self,
+        path: &str,
+        data: &str,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        self.storage.write(path, data).await?;
+        Ok(())
+    }
+}
